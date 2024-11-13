a3060c175423a2c2325bea6c28ec7c577baaba57
gizemcoban <gizem.coban.r@gmail.com>
2024-11-13 12:08:59 +0300

    kafe app ui is working and localstorage are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Options from './components/Options';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === 'reset') {
+      // Reset seçeneği ile tüm durumları sıfırlarız
+      const resetFeedback ={ good: 0, neutral: 0, bad: 0 }
+      setFeedback(resetFeedback);
+      setLocalStorage(resetFeedback);
+
+    } else {
+      // Seçilen geri bildirim türüne göre durumu artırıyoruz
+      setFeedback((prevFeedback) =>{ 
+        const updatedFeedback={
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      };
+      setLocalStorage(updatedFeedback);
+      return updatedFeedback;
+    });
+    }
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  const setLocalStorage=(data)=>{
+    localStorage.setItem("feedbackData",JSON.stringify(data));
+    }
+
+  const getLocalStorage=()=>{
+    const localData = localStorage.getItem("feedbackData")
+    if (localData) {
+    setFeedback(JSON.parse(localData));
+    }
+  }
+
+  useEffect(() => {
+    getLocalStorage();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='description' style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
+        <h1 style={{marginBottom:0}}>Sip Happens Café</h1>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <Options onFeedback={updateFeedback}  />
+
+      <div className="feedback" style={{display:"flex",alignItems:"flex-start"}}>
+        <p className="feedback-text" style={{marginRight:"10px"}}>Good: {feedback.good}</p>
+        <p className="feedback-text" style={{marginRight:"10px"}}>Neutral: {feedback.neutral}</p>
+        <p className="feedback-text" style={{marginRight:"10px"}}>Bad: {feedback.bad}</p>
+        <p className="feedback-text" style={{marginRight:"10px"}}>Total: {totalFeedback}</p>
+        <p className="feedback-text" style={{marginRight:"10px"}}>Positive: {positiveFeedback}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ src/components/Options.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line react/prop-types
+function Options({ onFeedback }) {
+    return (
+      <div className="options" style={{display:"flex",alignItems:"flex-start"}}>
+        <button className="count-buttons" id="good" onClick={() => onFeedback("good")}>Good</button>
+        <button className="count-buttons" id="neutral" onClick={() => onFeedback("neutral")}>Neutral</button>
+        <button className="count-buttons" id="bad" onClick={() => onFeedback("bad")}>Bad</button>
+        <button className="count-buttons" id="reset" onClick={() => onFeedback("reset")}>Reset</button>
+
+      </div>
+    );
+  }
+  
+  export default Options;
